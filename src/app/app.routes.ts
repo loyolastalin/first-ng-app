ccd4994ec7ec13638eb241cc3bc6b7b62a576944
loyolastalin <loyolastalin@gmail.com>
2024-12-10 17:48:29 +0530

    Added Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
+    canActivate:[MsalGuard],
     loadComponent: () => {
       return import('./home/home.component').then((m) => m.HomeComponent);
     },
   },
   {
     path: 'todo',
+    canActivate:[MsalGuard],
     loadComponent : () => { return import('./todo/todo.component').then( (m) => m.TodoComponent)}
   }
 ];
